1e15270b3b012b27ca173f5606bacf1d9aadb814
nigelsmk <kahler.maynard@gmail.com>
2017-01-04 12:31:04 -0500

    created grid array

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -17,16 +17,27 @@ import {Pairing} from "../../models/pairing";
 export class RecipesPage {
 
   public recipes: Recipe[];
+  public grid: Recipe[][];
   public selected: Pairing[];
 
   constructor(public navCtrl: NavController, private navParams: NavParams, private recipesService: RecipesService) {
     this.recipes = [];
+    this.grid = [];
     this.selected = navParams.get('selected');
   }
 
   ngOnInit() {
     this.recipesService.recipes.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
+
+      // fill grid with rows of two
+      let y = 0;
+      for (let i = 0; i < recipes.length; i++) {
+        let x = i % 2;
+        this.grid[y][x] = recipes[i];
+        if (x == 1) y +=1;
+      }
+      console.log(this.grid);
     });
     this.recipesService.requestRecipes(this.selected);
 
